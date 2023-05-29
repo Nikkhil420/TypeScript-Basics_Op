44f2df91298547a6dc9cdc106a1045c2cd009604
Nikkhil420 <nikhilbujade419@gmail.com>
2023-05-29 17:36:27 +0530

    Main :: Basic Op

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ var func = function () {
     return console.log('addition', addition);
 };
 var ReturnValue = func();
+var funcSub = function (s, u) { return console.log(s - u); };
+var ReturnValueSub = funcSub(10, 5);
+var funcMulti = function (m, t) { return console.log(m * t); };
+var ReturnValueMulti = funcMulti(10, 5);
+var funcDivi = function (d, v) { return console.log(d / v); };
+var ReturnValueDiv = funcDivi(100, 5);
--- app.ts
+++ app.ts
@@ -11,4 +11,17 @@ const func = () => {
 
 const ReturnValue = func();
 
+const funcSub = (s:number , u:number) => console.log(s - u);
 
+
+const ReturnValueSub = funcSub(10,5);
+
+const funcMulti = (m:number , t:number) => console.log(m * t);
+
+
+const ReturnValueMulti = funcMulti(10,5);
+
+const funcDivi = (d:number , v:number) => console.log(d / v);
+
+
+const ReturnValueDiv = funcDivi(100,5);

3b35a8a3ebd311efdc6d7a4ce3b71b6936287181
Nikkhil420 <nikhilbujade419@gmail.com>
2023-05-29 17:27:53 +0530

    2nd commit Add

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Hello TypeScript');
+var func = function () {
+    var a = 12;
+    var b = 55;
+    var addition = a + b;
+    return console.log('addition', addition);
+};
+var ReturnValue = func();
--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('Hello TypeScript')
+const func = () => {
+
+  const a: number = 12;
+  const b: number = 55;
+
+  const addition:number = a + b;
+
+  return console.log('addition',addition);
+
+};
+
+const ReturnValue = func();
+
+
